e05fe3b25ed7dd9c8b61a44872b0a78d8429bad0
jasmineanove <jasmineanove@gmail.com>
2018-07-24 18:16:44 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,7 +1,6 @@
 def mothers_day("Mom")
-  puts "Whats your mothers name?"
-  your_Moms_Name = gets.strip
-  puts "Happy Mothers Day! #{your_Moms_Name}"
+  your_Moms_Name = "Mom"
+  puts "Happy Mothers Day, #{your_Moms_Name}!"
 end
 
 mothers_day
