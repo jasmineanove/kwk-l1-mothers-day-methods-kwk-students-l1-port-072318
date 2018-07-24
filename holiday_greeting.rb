2857b6b5d2fb3a3f98b60157d9bab71619c342fe
jasmineanove <jasmineanove@gmail.com>
2018-07-24 18:24:07 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,2 +1,5 @@
-def holiday greeting (name_1 = "beyonce", name_2 = "Jay-z", name_3 = "Fourth of July")
-  put "Happy #{name_3}, #{name_1}! - From #{name_2}
+def holiday_greeting (name_1 = "beyonce", name_2 = "Jay-z", name_3 = "Fourth of July")
+  put "Happy #{name_3}, #{name_1}! - From #{name_2}"
+end
+
+holiday_greeting
